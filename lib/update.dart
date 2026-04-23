cf06228b03dfc6d329b2f06f5690f0440a14fa63
mayunemo <mayookha.offical@gmail.com>
2026-04-23 17:13:56 +0530

    statusbarcompleted

--- lib/update.dart
+++ lib/update.dart
@@ -31,7 +31,9 @@ class Updates extends StatelessWidget {
         ],
       ),
 
-      body: Column(children: [ SizedBox(height: 20),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.only(left: 20),
             child: Align(
@@ -41,7 +43,105 @@ class Updates extends StatelessWidget {
                 style: TextStyle(color: Colors.white, fontSize: 25),
               ),
             ),
-          ),],),
+          ),
+          Row(
+            children: [
+              SizedBox(width: 15),
+              SizedBox(
+                height: 150,
+                width: 100,
+                child: Card(
+                  color: const Color.fromARGB(255, 46, 44, 44),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 50,
+                        left: 23,
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage("assets/shino.jpeg"),
+                        ),
+                      ),
+
+                      Positioned(
+                        top: 70,
+                        left: 45,
+                        child: CircleAvatar(
+                          radius: 10,
+                          child: Icon(Icons.add, size: 20, color: Colors.black),
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            37,
+                            149,
+                            40,
+                          ),
+                        ),
+                      ),
+                      Positioned(top: 110,left: 13,
+                        child: Text(
+                          "Add status",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 150,
+                width: 100,
+                child: Card(
+                  color: const Color.fromARGB(255, 46, 44, 44),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 10,
+                        left: 10,
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage("assets/poo.jpeg"),
+                        ),
+                      ),
+                      Positioned(
+                        top: 110,
+                        left: 25,
+                        child: Text(
+                          "amma",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 150,
+                width: 100,
+                child: Card(
+                  color: const Color.fromARGB(255, 46, 44, 44),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 10,
+                        left: 10,
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage("assets/pee.jpeg"),
+                        ),
+                      ),
+                      Positioned(
+                        top: 110,
+                        left: 25,
+                        child: Text(
+                          "achn",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
